19cbf78df4068571d8b74e9a8d2674525f71a286
Srivatsan Seshadri <srivatsan_seshadri@symbotic.com>
2021-08-27 16:41:25 +0530

    Assembly Concept covered.

---
+++ CSharpTraining/AccessPrivateAssembly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrivateAssembly;
+
+namespace CSharpTraining
+{
+   public class AccessPrivateAssembly
+    {
+        public static void Main(string[] args)
+        {
+            PrivateAssembly.Calculator calculator = new PrivateAssembly.Calculator();
+            Console.WriteLine(calculator.Add(10, 20));
+            Console.WriteLine(calculator.Subract(10, 20));
+            Console.WriteLine(calculator.Multiply(10, 20));
+            Console.WriteLine(calculator.Divide(10, 20));
+        }
+    }
+}
